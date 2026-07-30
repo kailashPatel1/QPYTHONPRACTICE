b68947a9ae580c5939b7af534760b344a598993d
Kailash Patel <kailash102004@gmail.com>
2026-07-30 22:06:57 +0530

    Added leap year program

--- ELIF.py
+++ ELIF.py
@@ -117,3 +117,9 @@
 
 
 # WAP to  check given leap year or not
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a Leap Year")
+else:
+    print(year, "is Not a Leap Year")
